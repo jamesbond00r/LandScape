f1dae2a8f5f39b95d8a3bfec5ab6057e849e012a
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-13 11:48:49 -0600

    Added About Home Contact Gallery and react routes

--- App/Components/App.js
+++ App/Components/App.js
@@ -4,14 +4,30 @@ var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
 var Navbar = require('./Navbar');
+var Home = require('./Home');
+var Contact = require('./Contact');
+var About = require('./About');
+var Gallery = require('./Gallery');
 
 class App extends React.Component{
 	render(){
 		return(
-
+			<Router>
 			<div>
-			<Navbar />
+				<Navbar />
+
+				<Switch>
+
+			<Route exact path='/' component={Home} />
+            <Route exact path='/About' component={About} />
+            <Route path='/Contact' component={Contact} />
+            <Route path='/Gallery' component={Gallery} />
+            <Route render={function () {
+              return <p>Not Found</p>
+            }} />
+			</Switch>
 			</div>
+			</Router>
 		)
 	}
 }
--- App/Components/Navbar.js
+++ App/Components/Navbar.js
@@ -1,15 +1,39 @@
 var React = require('react');
+var NavLink = require('react-router-dom').NavLink;
 
 class Navbar extends React.Component {
 	render(){
 
 	return(
+
 		<div className="navbar">
 		<ul>
-		<li>Home</li>
-		<li>About</li>
-		<li>Contact</li>
-		<li>Gallery</li>
+		
+		<li>
+		<NavLink exact activeClassName='active' to='/'>
+		Home
+		</NavLink>
+			</li>
+
+		<li>
+		<NavLink activeClassName='active' to='/About'>
+		About
+		</NavLink>
+		</li>
+
+		<li>
+		<NavLink activeClassName='active' to='/Contact'>
+		Contact
+		</NavLink>
+		</li>
+
+
+		<li>
+		<NavLink activeClassName='active' to='/Gallery'>
+		Gallery
+		</NavLink>
+		</li>
+
 		</ul>
 		</div>
 
