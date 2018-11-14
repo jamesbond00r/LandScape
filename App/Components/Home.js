68ead7818831e50a047f535fb3ec557f59dd72db
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-14 12:04:22 -0600

    Start building out body of Home Page

--- App/Components/Home.js
+++ App/Components/Home.js
@@ -13,7 +13,38 @@ class Home extends React.Component{
 			</div>
 			</div>
 			<div className="seeItFirst">
-			<h1>Our Services</h1>
+			<h1 className="ServicesHeader">Our Services</h1>
+			</div>
+			<div>
+			
+			
+			
+			</div>
+
+<div className="centered">
+
+			<div className="sections">
+
+			
+
+			<section className="sections">
+			
+			<img className="round" src={"https://images.pexels.com/photos/6083/garden-moss-stone-sett.jpg?auto=compress&cs=tinysrgb&h=650&w=940"}/>
+			<h4 className="services">Hardscape</h4>
+		</section>
+
+			<section className="sections">
+            <img className="round" src={"https://images.pexels.com/photos/413195/pexels-photo-413195.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}/>
+          	<h4 className="services">Yard Clean Up</h4>
+         </section>
+
+          <section className="sections">
+          	<img className="round" src={"https://images.pexels.com/photos/269063/pexels-photo-269063.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"}/>
+          <h4 className="services">Wood Work</h4>
+         </section>
+		
+
+			</div>
 			</div>
 			</div>
 		)
