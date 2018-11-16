89308d349cecea715c87c92c9c1f7c2446a2249b
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-15 19:50:22 -0600

    Build out gallery with placeholders

--- App/Components/Gallery.js
+++ App/Components/Gallery.js
@@ -5,7 +5,24 @@ class Gallery extends React.Component{
 		return(
 
 			<div>
-			Gallery
+			<div >
+
+
+<img className="Gallery" src={"https://images.pexels.com/photos/860564/pexels-photo-860564.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}/>
+<img className="Gallery" src={"https://images.pexels.com/photos/534171/pexels-photo-534171.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}/>
+<img className="Gallery" src={"https://images.pexels.com/photos/210531/pexels-photo-210531.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}/>
+
+
+<img className="Gallery" src={"https://images.pexels.com/photos/206673/pexels-photo-206673.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}/>
+<img className="Gallery" src={"https://images.pexels.com/photos/261410/pexels-photo-261410.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}/>
+<img className="Gallery" src={"https://images.pexels.com/photos/136097/pexels-photo-136097.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"}/>
+
+
+<img className="Gallery" src={"https://images.pexels.com/photos/449023/pexels-photo-449023.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"}/>
+<img className="Gallery" src={"http://c2.staticflickr.com/4/3888/14878097108_5997041006_b.jpg"}/>
+<img className="Gallery" src={"http://c2.staticflickr.com/8/7579/15482110477_0b0e9e5421_b.jpg"}/>
+
+	</div>
 			</div>
 		)
 	}
